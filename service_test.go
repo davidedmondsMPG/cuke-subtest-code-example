832f478b79866b15ba73cf86bf0864035bf79688
David Edmonds <davidedmonds@mpg.io>
2023-02-03 10:14:34 +0000

    example: two tests that demonstrate the subtest issue
    Issue is described in https://github.com/cucumber/godog/issues/536

---
+++ service_test.go
@@ -0,0 +1,54 @@
+package cuke_subtest_code_example
+
+import (
+	"testing"
+
+	"github.com/cucumber/godog"
+)
+
+func TestFeaturesNoSubtests(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format: "pretty",
+			Paths:  []string{"features"},
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
+
+func TestFeaturesSubtests(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t, // Testing instance that will run subtests.
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
+
+func iEat(arg1 int) error {
+	return godog.ErrPending
+}
+
+func thereAreGodogs(arg1 int) error {
+	return godog.ErrPending
+}
+
+func thereShouldBeRemaining(arg1 int) error {
+	return godog.ErrPending
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	ctx.Step(`^I eat (\d+)$`, iEat)
+	ctx.Step(`^there are (\d+) godogs$`, thereAreGodogs)
+	ctx.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
+}
